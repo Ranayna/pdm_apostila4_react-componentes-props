23554d270e2b83e39ef2ee6f9360924a23e643b0
Ranayna <ranayna-oliveira@outlook.com>
2022-10-14 20:12:40 -0300

    Apostila 4 de pdm finalizada com sucesso

---
+++ src/Feedback.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/* O componenete FeedBack será como um irmão do componenete Pedido no index*/
+const Feedback = props => {
+    return (
+    <div className="d-flex justify-content-evenly m-2">
+        <button type="button" onClick={props.funcaoOK} className="btn btn-primary">{props.textoOK}</button>
+        <button type="button" onClick={props.funcaoNOK} className="btn btn-danger">{props.textoNOK}</button>
+    </div>
+    )
+}
+export default Feedback;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import Pedido from './Pedido'
+import Cartao from './Cartao'
+import Feedback from './Feedback'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/* Sobre Children - filhos
+Representa os elementos “filhos” de um Cartao, especificados no momento de seu uso. 
+sendo assim, quando a função cartão é chamada, o conteudo entre suas tags de abertura e fechamento fica associado 
+a uma chave chamada children
+*/ 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+    const textoOK = "Já chegou"
+    const textoNOK = "Ainda não chegou"
+    const funcaoOK = () => alert ("Agradecemos a confirmação!")
+    const funcaoNOK = () => alert ("Verificaremos o ocorrido!")
+    const componenteFeedback = <Feedback textoOK={textoOK} funcaoOK={funcaoOK} 
+    textoNOK={textoNOK} funcaoNOK={funcaoNOK}/>;
+   
+    return (
+    //container principal
+    <div className="container border rounded mt-2">
+        {/* linha para titulo */}
+        <div className="row border-bottom m-2">
+            <h1 className="display-5 text-center">Seus pedidos</h1>
+        </div>    
+
+        {/* linha para o primeiro pedido */}
+        <div className="row">
+            {/* controle de colunas para responsividade */}
+            <div className="col-sm-8 col-md-6 m-2">
+            <Cartao cabecalho="22/04/2021">
+                {/* children */}
+                <Pedido icone="fas fa-hdd fa-2x" titulo="SSD" descricao="SSD Kingston A400 - SATA"/>
+                {componenteFeedback}
+            </Cartao>      
+            </div>
+        </div>
+
+        {/* linha para o segundo pedido*/}
+        <div className="row">
+            {/* controle de colunas para responsividade*/}
+            <div className="col-sm-8 col-md-6 m-2">
+            <Cartao cabecalho="20/04/2021">
+                {/* children */}
+                <Pedido icone="fas fa-book fa-2x" titulo="Livro" 
+                descricao="Concrete Mathematics - Donald Knuth" />
+                {componenteFeedback}
+            </Cartao>
+            </div>
+        </div>
+                
+        {/* linha para o terceiro pedido*/}
+        <div className="row">
+            {/* controle de colunas para responsividade*/}
+            <div className="col-sm-8 col-md-6 m-2">
+            <Cartao cabecalho="21/01/2021">
+                 {/* children */}
+                <Pedido icone="fas fa-laptop fa-2x" titulo="Notebook" descricao="Notebook 
+                Dell - 8Gb - i5" />
+                {componenteFeedback}
+            </Cartao>
+            </div>
+        </div>
+    </div>
+    );
+}
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
